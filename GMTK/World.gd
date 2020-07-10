d6d47bb68b87be345853074b713cbf72d4cb1a78
Jason Ieong <zljieong@connect.ust.hk>
2020-07-11 05:56:38 +0800

    update character

--- GMTK/World.gd
+++ GMTK/World.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-onready var elf = $Elf
-onready var knight = $Knight
-onready var wizzard = $Wizzard
+onready var player = $Player
 
-onready var controlable = [elf, knight, wizzard]
 var controlling: int = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -25,8 +22,10 @@ func get_input()->Vector2:
 	return velocity
 
 func _process(delta):
-	print(get_input())
-	controlable[controlling].set_position(controlable[controlling].get_position()+ get_input()*16 )
+	player.move_and_collide( get_input() )
+#	get_position()+ get_input()*16 )
+#	print(get_input())
+#	controlable[controlling].set_position(controlable[controlling].get_position()+ get_input()*16 )
 #	if Input.is_action_pressed("ui_right"):
 #		controlable[controlling].set_position(controlable[controlling].get_position()+ Vector2(16,0) ) 
 #	pass
